23942f1840836862dd7494fe2da00e169f587c59
mlsamuelson <mlsamuel@asu.edu>
2018-05-11 11:50:31 -0700

    CLEAN UP all the commented out code we no longer need as reference or to pivot towards. Remove the DefaultController.php which DrupalModuleUpdater created, and consolidate the code there into the more semantically named HaxModeController.php. Remove unused HAXService.php copy left by the auto updater. Remove the upgrade-info.html left by the updater.

--- src/Controller/HaxModeController.php
+++ src/Controller/HaxModeController.php
@@ -5,16 +5,10 @@
 
 namespace Drupal\hax\Controller;
 
-use Drupal\Core\Access\AccessResult;
-use Drupal\Core\Controller\ControllerBase;
-
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\node\Controller\NodeViewController;
-//use Drupal\Core\Entity\Controller\EntityViewController;
-use Drupal\Core\Entity\EntityManagerInterface;
-use Drupal\Core\Render\RendererInterface;
-use Drupal\Core\Session\AccountInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Access\AccessResult;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Defines a controller to render a single node in HAX Mode.
@@ -26,77 +20,14 @@ class HaxModeController extends NodeViewController {
    */
   public function hax_node_form(EntityInterface $node, $view_mode = 'full', $langcode = NULL) {
     // Based on NodeViewController's view() method.
-
     $build = parent::view($node, $view_mode, $langcode);
 
-// TODO maybe just route to the canonical if we end up not actually using this controller.
-return $build;
-
-    // TODO NOTES
     // This method only seems useful for adding attachments, but not for
     // altering. Much of the contents of $build['#node'] are protected
     // Is hax_node_view() a better place for altering the node field output?
     // Or are there other hooks we're missing?
 
-
-    // Add HAX components to node.
-
-    // Only apply on full view mode.
-    if ($build['#view_mode'] == 'full') {
-
-      //dpm($build);
-      //dpm($build['#node']);
-      //dpm($build['#node']->id());
-
-      // fake a component to get it into the head of the document, heavy weighting
-      $component = new \stdClass();
-      $component->machine_name = 'cms-hax';
-      $component->file = 'libraries/webcomponents/polymer/bower_components/cms-hax/cms-hax.html';
-      $element = [
-        '#tag' => 'link', // The #tag is the html tag
-        '#attributes' => [ // Set up an array of attributes inside the tag
-          'href' => base_path() . $component->file,
-          'rel' => 'import',
-        ],
-      ];
-      $build['#attached']['html_head'][] = [
-        $element,
-        'webcomponent-' . $component->machine_name
-      ];
-
-
-      /* This stuff is being added in hax_node_view().
-
-          // generate autoload list
-          $elementstring = \Drupal::config('hax.settings')->get('hax_autoload_element_list');
-          // blow up based on space
-          $elements = explode(' ', $elementstring);
-          $components = '';
-          foreach ($elements as $element) {
-            // sanity check
-            if (!empty($element)) {
-              $components .= '<' . $element . ' slot="autoloader">' . '</' . $element . '>';
-            }
-          }
-          $appStoreConnection = array(
-            'url' => base_path() . 'hax-app-store/' . \Drupal::csrfToken()->get(),
-          );
-          // write content to screen, wrapped in tag to do all the work
-          $content = '
-          <cms-hax open-default end-point="' . base_path() . 'hax-node-save/' . $node->id() . '/' . \Drupal::csrfToken()->get() . '" body-offset-left="' . \Drupal::config('hax.settings')->get('hax_offset_left') . '" app-store-connection=' . "'" . json_encode($appStoreConnection) . "'" . '>'
-          . $components .
-            check_markup($node->body[0]->value, $node->body[0]->format)
-          .'</cms-hax>';
-
-          $response = new \Symfony\Component\HttpFoundation\Response();
-          $response->setContent($content);
-          $response->setMaxAge(1);
-          error_log('in hax\DefaultController\_hax_node_form');
-          return $response;
-      */
-
-    }
-
+    // TODO maybe just route to the canonical if we end up not actually using this controller.
     return $build;
   }
 
@@ -110,9 +41,151 @@ return $build;
    *   The page title.
    */
   public function title(EntityInterface $node) {
-    // TODO - doesn't appear to be working, but shows up in router. What gives?
-    // TODO - make translatable
-    return "HAX edit ". $this->entityManager->getTranslationFromContext($node)->label();
+    // TODO Doesn't appear to be working, but shows up in router. What gives?
+    return t("HAX edit ") . $this->entityManager->getTranslationFromContext($node)->label();
+  }
+
+
+  /**
+   * Permission + Node access check.
+   */
+  public function _hax_node_access($op, \Drupal\node\NodeInterface $node) {
+
+
+    if (\Drupal::currentUser()->hasPermission('use hax') && node_node_access($node, $op, \Drupal::currentUser())) {
+      return AccessResult::allowed();
+    }
+    return AccessResult::forbidden();
+  }
+
+  public function _hax_node_save(\Drupal\node\NodeInterface $node, $token) {
+
+    if ($_SERVER['REQUEST_METHOD'] == 'PUT' && \Drupal::csrfToken()->validate($token)) {
+
+      // We're not using the Drupal entity REST system outright here, as PUT
+      // isn't supported. But we can, ahem, "patch" the behavior in ourselves.
+
+      // HAX submitted value is right here.
+      $body = file_get_contents('php://input');
+
+      // Get the current format, and retain it. User will have to manage that
+      // via the edit tab. We don't want to auto-set it. Making changes like
+      // that without user intentionality is bad practice.
+      $current_format = $node->get('body')->getValue()[0]['format'];
+
+      // TODO Should we leverage the Text Editor API ?
+      // https://www.drupal.org/docs/8/api/text-editor-api/overview
+
+      // TODO Any santization or security checks on $body?
+      $node->get('body')->setValue(['value' => $body, 'format' => $current_format]);
+      $node->save();
+
+      // Build the response object.
+      $response = new Response;
+      $response->headers->set('Content-Type', 'application/json');
+      $response->setStatusCode(200);
+      $response->setContent(json_encode([
+        'status' => 200,
+        'message' => 'Save successful',
+        'data' => $node,
+      ]));
+      return $response;
+    }
+
+    $response = new Response;
+    $response->headers->set('Content-Type', 'application/json');
+    $response->setStatusCode(403);
+    $response->setContent(json_encode([
+      'status' => 403,
+      'message' => 'Unauthorized',
+      'data' => NULL,
+    ]));
+    return $response;
+  }
+
+  /**
+   * Permission + File access check.
+   */
+  public function _hax_file_access($op) {
+    // FIXME entity_access bit in next line needs to be updated for D8
+    if (\Drupal::currentUser()->hasPermission('use hax') && entity_access('create', 'file', $_FILES['file-upload']['type'])) {
+      return AccessResult::allowed();
+    }
+    return AccessResult::forbidden();
+  }
+
+  /**
+   * Save a file to the file system.
+   */
+  public function _hax_file_save($token) {
+    $status = 403;
+    $return = '';
+
+    // check for the uploaded file from our 1-page-uploader app
+    // and ensure there are entity permissions to create a file of this type
+    // FIXME entity_access bit in next line needs to be updated for D8
+    if (\Drupal::csrfToken()->validate($token) && isset($_FILES['file-upload']) && entity_access('create', 'file', $_FILES['file-upload']['type'])) {
+      $upload = $_FILES['file-upload'];
+      // check for a file upload
+      if (isset($upload['tmp_name']) && is_uploaded_file($upload['tmp_name'])) {
+        // get contents of the file if it was uploaded into a variable
+        $data = file_get_contents($upload['tmp_name']);
+        $params = filter_var_array($_GET, FILTER_SANITIZE_STRING);
+        // see if we had a file_wrapper defined, otherwise this is public
+        if (isset($params['file_wrapper'])) {
+          $file_wrapper = $params['file_wrapper'];
+        }
+        else {
+          $file_wrapper = 'public';
+        }
+        // see if Drupal can load from this data source
+        if ($file = file_save_data($data, $file_wrapper . '://' . $upload['name'])) {
+          file_save($file);
+          $file->url = file_create_url($file->uri);
+          $return = ['file' => $file];
+          $status = 200;
+        }
+      }
+    }
+
+    // Build the response object.
+    $response = new Response;
+    $response->headers->set('Content-Type', 'application/json');
+    $response->setStatusCode($status);
+    $response->setContent(json_encode([
+      'status' => $status,
+      'data' => $return,
+    ]));
+    return $response;
+  }
+
+  public function _hax_load_app_store($token) {
+    // Ensure we had data PUT here and it is valid
+    if (\Drupal::csrfToken()->validate($token)) {
+
+      // Hooks and alters.
+      $appStore = \Drupal::moduleHandler()->invokeAll('hax_app_store');
+      \Drupal::moduleHandler()->alter('hax_app_store', $appStore);
+      $staxList = \Drupal::moduleHandler()->invokeAll('hax_stax');
+      \Drupal::moduleHandler()->alter('hax_stax', $staxList);
+
+      // Send the Response object with Apps and StaxList.
+      $response = new Response;
+      $response->headers->set('Content-Type', 'application/json');
+      $response->setStatusCode(200);
+      $response->setContent(json_encode([
+        'status' => 200,
+        'apps' => $appStore,
+        'stax' => $staxList
+      ]));
+      return $response;
+
+    }
+
+    // "Unauthorized" response.
+    $response = new Response;
+    $response->setStatusCode(403);
+    return $response;
   }
 
 }
